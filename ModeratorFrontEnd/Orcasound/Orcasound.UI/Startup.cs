021b466e60cc140cdb3a81dda9cd86a223bde0db
Mike Cowan <micowan@microsoft.com>
2020-10-17 20:52:30 +0000

    Merged PR 4835: UI authentication is done, default order put back to Timestamp, submit button...
    UI authentication is done, default order put back to Timestamp, submit button cleanup

--- ModeratorFrontEnd/Orcasound/Orcasound.UI/Startup.cs
+++ ModeratorFrontEnd/Orcasound/Orcasound.UI/Startup.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +23,26 @@ namespace Orcasound.UI
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+				.AddAzureAD(options => Configuration.Bind("AzureAd", options));
+
+			services.AddControllersWithViews();
+
+			services.AddAuthorization(options =>
+			{
+				options.AddPolicy("ModeratorGroupRole", policyBuilder =>
+					policyBuilder.RequireClaim("groups", "bb64ad99-f25d-4f08-a61b-38851ac3d263"));
+			});
+
+			//services.AddControllersWithViews(options =>
+			//{
+			//	var policy = new AuthorizationPolicyBuilder()
+			//		.RequireClaim("groups",
+			//			Configuration.GetValue<string>("AzureADGroup:ModeratorGroupId"))
+			//		.Build();
+			//	options.Filters.Add(new AuthorizeFilter(policy));
+			//});
+
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
 
@@ -30,6 +52,8 @@ namespace Orcasound.UI
 
 				// client.BaseAddress = new Uri("https://moderatorcandidates.azurewebsites.net/");
 			});
+
+			services.AddHttpContextAccessor();
 
 			// Optional for debugging
 			services.AddServerSideBlazor(o => o.DetailedErrors = true);
@@ -45,14 +69,21 @@ namespace Orcasound.UI
 			else
 			{
 				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
 			}
 
+			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
 			app.UseRouting();
 
+			app.UseAuthentication();
+			app.UseAuthorization();
+
 			app.UseEndpoints(endpoints =>
 			{
+				endpoints.MapControllers();
 				endpoints.MapBlazorHub();
 				endpoints.MapFallbackToPage("/_Host");
 			});
