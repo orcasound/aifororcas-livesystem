ba4117768ef66c5a445ec71d38317cfa4cb4a20e
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2021-10-13 19:48:03 +0530

    Add create_pull_request() function to programmatically create PR

---
+++ InferenceSystem/src/hls_utils/gh_utils.py
@@ -0,0 +1,12 @@
+import requests
+import json
+import os
+
+token = os.getenv('GITHUB_ACCESS_TOKEN')
+
+def create_pull_request(title, head, base, body):
+    url = "https://api.github.com/repos/orcasound/aifororcas-livesystem/pulls"
+    headers = {"Authorization": "token {}".format(token)}
+    data = {"title": title, "head": head, "base": base, "body": body}
+
+    res = requests.post(url=url, data=json.dumps(data), headers=headers)
