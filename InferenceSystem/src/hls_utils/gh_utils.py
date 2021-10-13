77a512a244dffeee0f8d747d8a62474d454109e3
Dhananjay Purohit <dhananjaypurohit7@gmail.com>
2021-10-13 22:38:03 +0530

    Update authorization header format
    Co-authored-by: Dmitry Volodin <mr.molkree@gmail.com>

--- InferenceSystem/src/hls_utils/gh_utils.py
+++ InferenceSystem/src/hls_utils/gh_utils.py
@@ -6,7 +6,7 @@ token = os.getenv('GITHUB_ACCESS_TOKEN')
 
 def create_pull_request(title, head, base, body):
     url = "https://api.github.com/repos/orcasound/aifororcas-livesystem/pulls"
-    headers = {"Authorization": "token {}".format(token)}
+    headers = {"Authorization": f"token {token}"}
     data = {"title": title, "head": head, "base": base, "body": body}
 
     res = requests.post(url=url, data=json.dumps(data), headers=headers)
