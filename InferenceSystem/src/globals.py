f2544707e3b60cd44a98c2c975a3f76f04bd1e4b
Patrick Pastore <ppastore@microsoft.com>
2021-10-14 16:31:55 -0700

    Improvements per PR comments

--- InferenceSystem/src/globals.py
+++ InferenceSystem/src/globals.py
@@ -1,9 +1,9 @@
 import datetime
 
 S3_STREAM_URLS = {
-    'orcasound_lab':'https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_orcasound_lab',
-    'port_townsend':'https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_port_townsend',
-    'bush_point':'https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_bush_point'
+    "orcasound_lab": "https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_orcasound_lab",
+    "port_townsend": "https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_port_townsend",
+    "bush_point": "https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_bush_point",
 }
 
-MAX_DATETIME_DELTA = datetime.datetime.timedelta(minutes=30)
+MAX_DATETIME_DELTA = datetime.timedelta(minutes=30)
