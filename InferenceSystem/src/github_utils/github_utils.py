dab2c0b0eb2c020acd09486a66f3acf7504789ba
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2021-10-14 09:57:49 +0530

    Add doc string to the function and rename & move the function file

--- InferenceSystem/src/github_utils/github_utils.py
+++ InferenceSystem/src/github_utils/github_utils.py
@@ -4,6 +4,14 @@ import os
 
 token = os.getenv('GITHUB_ACCESS_TOKEN')
 
+"""Creates pull request within orcasound/aifororcas-livesystem.
+
+parameters:
+title -- The title of pull request
+head -- The name of the branch where your changes are implemented
+base -- The name of the branch you want the changes pulled into
+body -- The description or contents of the pull request
+"""
 def create_pull_request(title, head, base, body):
     url = "https://api.github.com/repos/orcasound/aifororcas-livesystem/pulls"
     headers = {"Authorization": f"token {token}"}
