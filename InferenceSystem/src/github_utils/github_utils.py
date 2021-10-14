e8021c47b62a41699679e368a940b010833c490d
Dhananjay Purohit <dhananjaypurohit7@gmail.com>
2021-10-14 21:53:00 +0530

    Format doc string
    Co-authored-by: Dmitry Volodin <mr.molkree@gmail.com>

--- InferenceSystem/src/github_utils/github_utils.py
+++ InferenceSystem/src/github_utils/github_utils.py
@@ -4,15 +4,15 @@ import os
 
 token = os.getenv('GITHUB_ACCESS_TOKEN')
 
-"""Creates pull request within orcasound/aifororcas-livesystem.
-
-parameters:
-title -- The title of pull request
-head -- The name of the branch where your changes are implemented
-base -- The name of the branch you want the changes pulled into
-body -- The description or contents of the pull request
-"""
 def create_pull_request(title, head, base, body):
+    """Creates pull request within orcasound/aifororcas-livesystem.
+
+    Args:
+        `title`: The title of pull request
+        `head`: The name of the branch where your changes are implemented
+        `base`: The name of the branch you want the changes pulled into
+        `body`: The description or contents of the pull request
+    """
     url = "https://api.github.com/repos/orcasound/aifororcas-livesystem/pulls"
     headers = {"Authorization": f"token {token}"}
     data = {"title": title, "head": head, "base": base, "body": body}
