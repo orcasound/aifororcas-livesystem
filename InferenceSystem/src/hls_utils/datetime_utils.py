0b0feaa34e9780e84c68c48197c37d650ffd1fea
Prakruti Gogia <prakruti.gogia@gmail.com>
2022-11-26 06:04:49 +0000

    Datetime utils changes

--- InferenceSystem/src/hls_utils/datetime_utils.py
+++ InferenceSystem/src/hls_utils/datetime_utils.py
@@ -3,13 +3,16 @@ from datetime import datetime
 from pytz import timezone
 from datetime import timedelta
 
-def get_clip_name_from_unix_time(source_guid, current_clip_start_time):
+def get_clip_name_in_pst_from_unix_time(source_guid, current_clip_start_time):
     """
 
     """
 
     # convert unix time to 
-    readable_datetime = datetime.fromtimestamp(int(current_clip_start_time)).strftime('%Y_%m_%d_%H_%M_%S')
+    dt1_utc = datetime.utcfromtimestamp(int(current_clip_start_time))
+    dt1_utc_aware = timezone('UTC').localize(dt1_utc)
+    dt2_aware = dt1_utc_aware.astimezone(timezone('US/Pacific'))
+    readable_datetime = dt2_aware.strftime('%Y_%m_%d_%H_%M_%S')
     clipname = source_guid + "_" + readable_datetime
     return clipname, readable_datetime
 
@@ -20,11 +23,7 @@ def get_difference_between_times_in_seconds(unix_time1, unix_time2):
     return (dt1-dt2).total_seconds()
 
 def add_interval_to_unix_time(unix_time, interval_in_seconds):
-    dt1 = datetime.fromtimestamp(int(unix_time)) + timedelta(0, interval_in_seconds)
-    dt1_aware = timezone('US/Pacific').localize(dt1)
-    end_time_unix = int(dt1_aware.timestamp())
-
-    return end_time_unix
+    return int(unix_time) + int(interval_in_seconds)
 
 def get_unix_time_from_datetime_utc(dt_utc):
 
