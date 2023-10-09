fba5bca6a7298c940256fa2c2f6c1771791c5d75
Michael Cowan <micowan@microsoft.com>
2023-10-09 14:45:12 -0400

    DataMigration added to create new schema and copy rows around between online and local emulator.

---
+++ ModeratorFrontEnd/OrcaHello/OrcaHello.Console.DataMigration/Models/CurrentSchema.cs
@@ -0,0 +1,20 @@
+﻿namespace OrcaHello.Console.DataMigration.Models
+{
+    public class Metadata
+    {
+        public string id { get; set; }
+        public string source_guid { get; set; }
+        public string audioUri { get; set; }
+        public string imageUri { get; set; }
+        public bool reviewed { get; set; }
+        public DateTime timestamp { get; set; }
+        public decimal whaleFoundConfidence { get; set; }
+        public Location location { get; set; }
+        public List<Prediction> predictions { get; set; } = new List<Prediction>();
+        public string SRKWFound { get; set; }
+        public string comments { get; set; }
+        public string dateModerated { get; set; }
+        public string moderator { get; set; }
+        public string tags { get; set; }
+    }
+}
---
+++ ModeratorFrontEnd/OrcaHello/OrcaHello.Console.DataMigration/Models/Location.cs
@@ -0,0 +1,10 @@
+﻿namespace OrcaHello.Console.DataMigration.Models
+{
+    public class Location
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+    }
+}
