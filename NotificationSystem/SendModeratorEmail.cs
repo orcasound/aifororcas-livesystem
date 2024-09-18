f4ac5c9ffc53b9f2578fb06e939dca26fed55630
Yuvaraj <yudurais@microsoft.com>
2024-09-18 13:17:40 -0700

    Revert "Auto Format"
    This reverts commit 5733a59577b1d6df33d8ab4299c4160c92e29f46.

--- NotificationSystem/SendModeratorEmail.cs
+++ NotificationSystem/SendModeratorEmail.cs
@@ -14,62 +14,62 @@ using ComposableAsync;
 
 namespace NotificationSystem
 {
-	[StorageAccount("OrcaNotificationStorageSetting")]
-	public static class SendModeratorEmail
-	{
-		[FunctionName("SendModeratorEmail")]
-		public static async Task Run(
-			[CosmosDBTrigger(
-				databaseName: "predictions",
-				collectionName: "metadata",
-				ConnectionStringSetting = "aifororcasmetadatastore_DOCUMENTDB",
-				LeaseCollectionName = "leases",
-				LeaseCollectionPrefix = "moderator",
-				CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input,
-			[Table("EmailList")] CloudTable cloudTable,
-			ILogger log)
-		{
-			if (input == null || input.Count == 0)
-			{
-				log.LogInformation("No updated records");
-				return;
-			}
+    [StorageAccount("OrcaNotificationStorageSetting")]
+    public static class SendModeratorEmail
+    {
+        [FunctionName("SendModeratorEmail")]
+        public static async Task Run(
+            [CosmosDBTrigger(
+                databaseName: "predictions",
+                collectionName: "metadata",
+                ConnectionStringSetting = "aifororcasmetadatastore_DOCUMENTDB",
+                LeaseCollectionName = "leases",
+                LeaseCollectionPrefix = "moderator",
+                CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input,
+            [Table("EmailList")] CloudTable cloudTable,
+            ILogger log)
+        {
+            if (input == null || input.Count == 0)
+            {
+                log.LogInformation("No updated records");
+                return;
+            }
 
-			var newDocumentCreated = false;
-			DateTime? documentTimeStamp = null;
-			string location = null;
+            var newDocumentCreated = false;
+            DateTime? documentTimeStamp = null;
+            string location = null;
 
-			foreach (var document in input)
-			{
-				if (!document.GetPropertyValue<bool>("reviewed"))
-				{
-					newDocumentCreated = true;
-					documentTimeStamp = document.GetPropertyValue<DateTime>("timestamp");
-					location = document.GetPropertyValue<string>("location.name");
-					break;
-				}
-			}
+            foreach (var document in input)
+            {
+                if (!document.GetPropertyValue<bool>("reviewed"))
+                {
+                    newDocumentCreated = true;
+                    documentTimeStamp = document.GetPropertyValue<DateTime>("timestamp");
+                    location = document.GetPropertyValue<string>("location.name");
+                    break;
+                }
+            }
 
-			if (!newDocumentCreated)
-			{
-				log.LogInformation("No unreviewed records");
-				return;
-			}
+            if (!newDocumentCreated)
+            {
+                log.LogInformation("No unreviewed records");
+                return;
+            }
 
-			// TODO: make better email
-			string body = EmailTemplate.GetModeratorEmailBody(documentTimeStamp, location);
+            // TODO: make better email
+            string body = EmailTemplate.GetModeratorEmailBody(documentTimeStamp, location);
 
 			var timeConstraint = TimeLimiter.GetFromMaxCountByInterval(14, TimeSpan.FromSeconds(1));
 			var aws = new AmazonSimpleEmailServiceClient(RegionEndpoint.USWest2);
 			log.LogInformation("Retrieving email list and sending notifications");
-			foreach (var emailEntity in EmailHelpers.GetEmailEntities(cloudTable, "Moderator"))
-			{
+            foreach (var emailEntity in EmailHelpers.GetEmailEntities(cloudTable, "Moderator"))
+            {
 				await timeConstraint;
 				string emailSubject = string.Format("OrcaHello Candidate at location {0}", location);
-				var email = EmailHelpers.CreateEmail(Environment.GetEnvironmentVariable("SenderEmail"),
-					emailEntity.Email, emailSubject, body);
-				await aws.SendEmailAsync(email);
-			}
-		}
-	}
+                var email = EmailHelpers.CreateEmail(Environment.GetEnvironmentVariable("SenderEmail"),
+                    emailEntity.Email, emailSubject, body);
+                await aws.SendEmailAsync(email);
+            }
+        }
+    }
 }
